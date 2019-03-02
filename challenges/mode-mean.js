36d29a4c16a46aebd66a0d5f57849776dd4a1ac7
Tayvon Wright <tayvonwright@gmail.com>
2019-03-02 09:58:49 -0500

    completeed mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,27 @@
 
 
 function modemean(array) {
-
+    let mode = [];
+    let seen = [];
+    let mean = 0;
+    // iterate through array to gather necessary values
+    for (let i = 0; i < array.length; i++){
+        if (!seen.includes(array[i])) seen.push(array[i]);
+        else mode.push(array[i]);
+        mean += array[i];
+    }
+    // calculate mean
+    mean = Math.floor(mean / array.length);
+    
+    // calculate mode
+    mode = mode.length === 0 ? Math.max(...seen) : Math.max(...mode)
+    
+    // return whether of not mode is equal to mean
+    return mode === mean;
 }
+
+// console.log(modemean([3,3,3]))
+// console.log(modemean([0,2,3,4,5]))
+// console.log(modemean([0,3,3,4,5]))
 
 module.exports = modemean;
