f3de2205f61a84c45d6759a8ec930e6bc94a2b64
Bo Peng <pb0928@gmail.com>
2019-03-02 09:55:23 -0500

    done with mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,26 @@
 
 
 function modemean(array) {
-
+    const mean = Math.floor(array.reduce((sum, item) => {
+        return sum += item;
+    },0)/array.length);
+    const data = array.reduce((o, item) => {
+        if(!(item in o)) o[item] = 1;
+        else o[item]++;
+        return o;
+    },{});
+    const mode = { key: 0, value: 0 };
+    for(let key in data) {
+        if(data[key] >= mode.value) {
+            mode.value = data[key];
+            mode.key = key;
+        }
+    }
+    if(mode.key === mean) return true;
+    else return false;
+    //const sum = array.reduce((sum, item) => { return sum += item; })
+    //console.log('mode:', mode.key, 'mean:', mean, 'sum:',sum,'/',array.length);
 }
+//modemean([1,2,3,4,4,4,5,6,6,7,7]);
 
 module.exports = modemean;
