ae54566ee7f9ef8cce339f4c272f2a33b68671ae
luijocroussett <ljcroussett@gmail.com>
2019-03-02 10:22:33 -0500

    completed mean mode challege

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,31 @@
 
 
 function modemean(array) {
-
+    let mode = {
+        number: null,
+        value: null
+            }
+    let mean = 0
+    let numbersObject = {} ;
+    for (let index in array) {
+        if (Object.keys(numbersObject).includes(array[index].toString())) numbersObject[array[index]] += 1;
+        else numbersObject[array[index]] = 1;
+    }
+    console.log(numbersObject)
+    for (let key in numbersObject) {
+        if (mode.value === null || numbersObject[key] >= mode.value) {
+            mode.value = numbersObject[key];
+            if (key > mode.number)  mode.number = key;
+        }
+    }
+    console.log(mode)
+    mean = array.reduce((cum, current) => {return cum + current;}) / array.length;
+    console.log(mean)
+    if (mean.toString() === mode.number) return true;
+    else return false;
 }
+
+console.log(modemean([1,1,2,3,4,7]))
+console.log(modemean([1,2,2,3]))
 
 module.exports = modemean;
