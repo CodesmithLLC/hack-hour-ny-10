6543ad76218051113e28cdae683d5d573cd5606f
Akira Sane <akirasane@Akiras-MacBook-Pro.local>
2019-03-06 09:59:32 -0500

    exp recursion pos and neg

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,15 @@
  */
 
 function pow(base, power) {
-
+    if (power === 0) return 1
+    if (power > 0) {
+        return base *= pow(base, power -1)
+    } else if (power < 0) {
+        return 1/(base *= pow(base, power +1))
+        
+    }
 }
+
+console.log(pow(2, -3))
 
 module.exports = pow;
