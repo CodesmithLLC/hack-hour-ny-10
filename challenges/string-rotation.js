27c1742ee4738a75ed4ca54d2e3302e6cc62e829
Bo Peng <pb0928@gmail.com>
2019-03-09 09:35:08 -0500

    done with string-rotation

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -15,8 +15,30 @@ function isSubstring(s1, s2) {
   return s1.indexOf(s2) >= 0;
 }
 
-function stringRotation(s1, s2) {
-
+function rotate(str) {
+  const temp = str[str.length-1];
+  const arr = str.split('');
+  for(let i = str.length-1; i > 0; i--) {
+    arr[i] = arr[i-1];
+  }
+  arr[0] = temp;
+  return arr.join('');
 }
+
+function stringRotation(s1, s2) {
+  if(isSubstring(s1, s2)) return true;
+  if(s1.length !== s2.length) return false;
+  let str = s1;
+  for(let i = 0; i < s1.length-1; i++) {
+    str = rotate(str);
+    if(isSubstring(str,s2)) return true;
+  }
+  return false;
+}
+console.log(stringRotation('hello','hello'));
+console.log(stringRotation('hello','llohe'));
+console.log(stringRotation('hello','elloh'));
+console.log(stringRotation('hello','ollhe'));
+console.log(stringRotation('he','eh'));
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
