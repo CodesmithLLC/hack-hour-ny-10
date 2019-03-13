c60893852419232d4493f4ee5f2959422c4c2e32
Tolga Mizrakci <tolgamizrakci@Tolgas-MacBook-Pro.local>
2019-03-13 10:11:10 -0400

    finished

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -17,8 +17,17 @@
  * 
  */
 
-function romanNumeral(n) {
-
-}
+function romanNumeral(num) {
+    var roman = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}
+    var result = ''
+  
+    for (var i in roman) {
+      while (num >= roman[i]) {
+        result += i
+        num -= roman[i]
+      }
+    }
+    return result
+  }
 
 module.exports = romanNumeral;
