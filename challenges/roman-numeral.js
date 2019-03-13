88390877f64581b0c8a43d45ac82c22ab141772e
luijocroussett <ljcroussett@gmail.com>
2019-03-13 09:57:09 -0400

    Complete romans hack hour

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,33 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
 function romanNumeral(n) {
-
+  let romanObj = {
+    1: "I",
+    4: "IV",
+    5: "V",
+    9: "IX",
+    10: "X",
+    40: "XL",
+    50: "L",
+    90: "XC",
+    100: "C",
+    400: "CD",
+    500: "D",
+    900: "CM",
+    1000: "M"
+  };
+  let romanNumber = "";
+  for (let index = Object.keys(romanObj).length - 1; index >= 0; index--) {
+    while (n >= parseInt(Object.keys(romanObj)[index])) {
+      n -= parseInt(Object.keys(romanObj)[index]);
+      romanNumber += romanObj[Object.keys(romanObj)[index]];
+    }
+  }
+  return romanNumber;
 }
 
 module.exports = romanNumeral;
