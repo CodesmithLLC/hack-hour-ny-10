2d5c1f15228c9593ea653341d52cd57ff1017d5c
Tolga Mizrakci <tolgamizrakci@Tolgas-MacBook-Pro.local>
2019-03-15 10:03:48 -0400

    finished

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -8,8 +8,37 @@
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
  */
 
-function subsetSum(array, target) {
+function subsetSum(arr, target) {
+    let i, j, temp
+    let result = []
+    let arrLen = arr.length
+    let power = Math.pow
+    let combinations = power(2, arrLen)
+    
+    for (i = 0; i < combinations;  i++) {
+      temp = []
+      for (j = 0; j < arrLen; j++) {
+        
+        if ((i & power(2, j))) {
+            temp.push(arr[j])
+        }
+      }
+      console.log(temp)
+      
+      if (temp.length) {
+        let tempSum = temp.reduce(function (accumulator, currentValue) {return accumulator + currentValue;}, 0);
 
+        console.log(tempSum)
+
+        if (tempSum === target) {
+          console.log(temp)
+          return true;
+        }
+      }
+      result.push(temp)
+    }
+    console.log(result)
+    return false
 }
 
 module.exports = subsetSum;
