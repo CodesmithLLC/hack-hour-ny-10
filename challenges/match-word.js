388e7285dba8cc1e29c65829512b4fcfca729578
Bo Peng <pb0928@gmail.com>
2019-03-19 10:26:38 -0400

    done with match word

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -11,7 +11,62 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
-
+    const regex = RegExp('[a-zA-Z]');
+    const parens = RegExp('[\(|\)|\{|\}|\[|\}|\]]');
+    const obj = { '(':')', '{':'}', '[':']' }
+    const stack = [];
+    let word = '';
+    for(let i = 0; i < str.length; i++) {
+        if(regex.test(str[i]) || parens.test(str[i])) {
+            if(regex.test(str[i])) {
+                word += str[i];
+                if(i === str.length-1) {
+                    if(stack.length !== 0) {
+                        const item = stack[stack.length-1];
+                        if(word.toLowerCase() === item.split('').reverse().join('')) stack.pop();
+                        else stack.push(word.toLowerCase());
+                    }
+                }
+            } else {
+                console.log(str[i]);
+                if(word !== '') {
+                    const w = word.toLowerCase();
+                    word = '';
+                    if(stack.length === 0) stack.push(w);
+                    else {
+                        const item = stack[stack.length-1];
+                        if(w === item.split('').reverse().join('')) stack.pop();
+                        else stack.push(w); 
+                    }
+                }
+                if(stack.length === 0) stack.push(str[i]);
+                else {
+                    const item = stack[stack.length-1];
+                    if(obj[item] === str[i]) stack.pop();
+                    else stack.push(str[i]);
+                }
+            }
+        } else {
+            if(word !== '') {
+                const w = word.toLowerCase();
+                word = '';
+                if(stack.length === 0) stack.push(w);
+                else {
+                    const item = stack[stack.length-1]; 
+                    if(w === item.split('').reverse().join('')) stack.pop();
+                    else stack.push(w);
+                }
+            }
+        }
+    }
+    if(stack.length === 0) return true;
+    else return false;
 }
+// console.log(matchWord('__END_DNE-----'));
+// console.log(matchWord('__ENDDNE__'));
+// console.log(matchWord('IF()()fi[]'));
+// console.log(matchWord('for__if__rof__fi'));
+// console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));
+// console.log(matchWord(''));
 
 module.exports = matchWord;
