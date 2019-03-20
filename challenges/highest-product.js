11ab51865481a9335c068de39864cc092db91b17
luijocroussett <ljcroussett@gmail.com>
2019-03-20 09:49:57 -0400

    completed highest-product challenge

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,24 @@
  */
 
 function highestProduct(array) {
-
+    let numbers = [array[0],array[1], array[2]].sort((a,b) => a-b);
+    console.log(numbers)
+    for (let index = 3; index < array.length; index++){
+        console.log(array[index])
+        if (array[index] > numbers[2]){
+            numbers.shift();
+            numbers.push(array[index]);
+        }
+        else if (array[index] > numbers[1] && array[index] < numbers[2]){
+            numbers[0] = numbers[1];
+            numbers[1] = array[index];
+        }
+        else if (array[index] > numbers[0] && array[index] < numbers[1]){
+            numbers[2] = array[index];
+        }
+    }
+    console.log(numbers)
+    return numbers[0] * numbers[1] * numbers[2];
 }
-
 
 module.exports = highestProduct;
