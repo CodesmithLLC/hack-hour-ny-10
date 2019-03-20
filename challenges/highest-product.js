81017ead39cc8bf93f89ddfa46721fd8f46eb848
Adam <spincycle01@yahoo.com>
2019-03-20 09:58:42 -0400

    highest product attempt

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,24 @@
  */
 
 function highestProduct(array) {
+  if (array.length < 3 || !Array.isArray) return 0;
 
+  //create empty array
+  const productArr = [];
+
+  for(let i = 1; i <= 3; i++) {
+    let value = Math.max(...array);
+    let index = array.indexOf(value)
+    array.splice(index, 1)
+    productArr.push(value);
+    //console.log(productArr)
+  }
+  
+  return productArr[0] * productArr[1] * productArr[2];
 }
 
+const arr = [1, 2, 4, 1, 5]
+const arr2 = [1, 2, 4, 1, 5, 10, 32, 4, -1]
+console.log(highestProduct(arr2))   //result 40
 
 module.exports = highestProduct;
--- challenges/match-word.js
+++ challenges/match-word.js
@@ -10,8 +10,49 @@
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
 
+//check if beginning is reverse of end
+//check if space exists between them 
+
+
 function matchWord(str) {
+  let newStr = str.replace(/[~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g, ' ').toLowerCase();
+  console.log(newStr)
+  const arr = Array.from(newStr);
+  //if (arr.includes(" ")) {
+    //console.log(arr)
+    let wordCheckLeft = '';
+    let wordCheckRight = '';
+    let wrongCount = 0;
+    for (let i = 0; i < arr.length / 2; i++) {
+      let left = arr[i];
+      let right = arr[arr.length - 1 - i];
+      if (left === right) {
+        wordCheckLeft.concat(left);
+        wordCheckRight.concat(right);
+      } else {
+        wrongCount += 1;
+        break;
+      }
+    }
+
+    console.log('right', wordCheckRight, 'left', wordCheckLeft, 'wrong', wrongCount)
 
 }
+  //   // return true
+  //   }
+  // } else {
+  //   return false;
+  // }
+ //console.log(wordCheck)
+
+
+
+
+matchWord('__END_DNE-----');  //-> true
+// matchWord('__ENDDNE__');  //-> false       (not separated by a space)
+// matchWord('IF()()fi[]');  //-> true        (should be case-insensitive)
+// matchWord('for__if__rof__fi');  //-> false     not properly closed. like ( [) ] 
+// matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  //-> true
+// matchWord('');  //-> true
 
 module.exports = matchWord;
