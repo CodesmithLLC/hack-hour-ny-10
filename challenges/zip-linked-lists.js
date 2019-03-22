ad6784786c7c54bd51f207110ef7e3ca13bd3dcc
Tayvon Wright <tayvonwright@gmail.com>
2019-03-22 16:19:12 -0400

    zipHH

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,8 +11,8 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-  if (l2 === null && l1 !== null) return l1;
-  else if (l1 === null && l2 !== null) return l2;
+  if ((l2 === null && l1 !== null) || l2 === undefined) return l1;
+  else if ((l1 === null && l2 !== null) || l1 === undefined) return l2;
   else if (l1 === null && l2 === null) return;
   let tempList = l1.next
   l1.next = l2;
@@ -29,26 +29,14 @@ var node5 = node4.next = new Node('5');
 var node1b = new Node('6');
 var node2b = node1b.next = new Node('7');
 var node3b = node2b.next = new Node('8');
-// var node4b = node3b.next = new Node('9');
-// var node5b = node4b.next = new Node('10');
+var node4b = node3b.next = new Node('9');
+var node5b = node4b.next = new Node('10');
 
-let test = zip(node1, node1b);
+let test = zip(undefined,node1b);
 
 while(test){
   console.log(test.value)
   test = test.next;
 }
-/*
-1
-6
-2
-7
-3
-8
-4
-9
-5
-10
-*/
 
 module.exports = {Node: Node, zip: zip};
