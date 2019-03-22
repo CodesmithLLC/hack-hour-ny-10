078755052d5d8f9ef49e6308acb8ffd8ab797269
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-03-22 09:56:43 -0400

    first attempt

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,24 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+  if (!l1) return l2;
+  if (!l2) return l1;
+  let dummy1 = new Node();
+  let dummy2 = new Node();
+  dummy1.next = l1;
+  dummy2.next = l2;
+  let tail1 = dummy1.next;
+  let tail2 = dummy2.next;
+  let tempNext2 = tail2.next;
+  while (tail1 && tail2) {
+    let tempNext1 = tail1.next;
+    tempNext2 = tail2.next;
+    tail1.next = tail2;
+    tail2.next = tempNext1;
+    tail1 = tempNext1;
+    tail2 = tempNext2;
+  }
+  return dummy1.next;
+}
 
-module.exports = {Node: Node, zip: zip};
+module.exports = { Node: Node, zip: zip };
