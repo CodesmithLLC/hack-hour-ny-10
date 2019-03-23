0d9022d8af9f6ec6517a9718566928d1e8da2de4
Tayvon Wright <tayvonwright@gmail.com>
2019-03-23 10:57:50 -0400

    max-stackHH

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,35 @@
  */
 
 function Stack() {
-  // body...
+  this.contents = {}
+  this.index = this.maxIndex = 0;
+
+  this.push = function (val){
+    this.contents[this.index] = val;
+    this.maxIndex = this.contents[this.index] > this.contents[this.maxIndex] ? this.index : this.maxIndex;
+    return ++this.index;
+  }
+
+  this.pop = function () {
+    const poppedVal =  this.contents[this.index - 1];
+    if (!poppedVal) return;
+    delete this.contents[this.index - 1];
+    if (this.maxIndex == this.index - 1){
+      let max = -Infinity;
+      for (let valIndex in this.contents){
+        if (this.contents[valIndex] > max) {
+          max = this.contents[valIndex];
+          this.maxIndex = valIndex;
+        }
+      }
+    }
+    --this.index;
+    return poppedVal;
+  }
+
+  this.getMax = function () {
+    return this.contents[this.maxIndex];
+  }
 }
 
 module.exports = Stack;
