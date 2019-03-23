ae98bbedf35200cc85c4cf5c5e5ff671ee8c681e
luijocroussett <ljcroussett@gmail.com>
2019-03-23 08:54:51 -0400

    finished hack hour

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -9,8 +9,40 @@ function Node(val) {
   this.value = val;
   this.next = null;
 }
+// SOLUTION: NOT IN PLACE
 
 function zip(l1, l2) {
+  if (!l1 && !l2) return null
+  let zippedList = new Node(0);
+    if (l1 && l2) {
+      zippedList.value = l1.value;
+      zippedList.next = new Node(l2.value);
+      l1 = l1.next;
+      l2 = l2.next;
+      if (l1 || l2) {
+        zippedList.next.next = zip(l1,l2);
+      }
+    }
+    else if (l1 & !l2) {
+      zippedList.value = l1.value;
+      l1 = l1.next;
+      if (l1 || l2) {
+        zippedList.next.next = zip(l1,l2);
+      }
+    }
+    else if (!l1 && l2) {
+      zippedList.value = l2.value;
+      l2 = l2.next;
+      if (l1 || l2) {
+        zippedList.next.next = zip(l1,l2);
+      }
+    }
+  return zippedList;
 };
 
-module.exports = {Node: Node, zip: zip};
+// let l1 = new Node(1);
+// l1.next = new Node(2);
+// l1.next.next = new Node(3);
+// console.log(zip(l1, l1));
+
+// module.exports = {Node: Node, zip: zip};
