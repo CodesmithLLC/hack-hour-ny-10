7c38568f1d1ffa97aca93ea8f9ccaa4cfca3a583
Bo Peng <pb0928@gmail.com>
2019-03-23 10:14:50 -0400

    done with maxstack, working on ziplinkedlists

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,42 @@
  */
 
 function Stack() {
-  // body...
+	this.stack = {};
+	this.maxArray = [Number.MIN_SAFE_INTEGER];
+	this.max = this.maxArray[0];
+	this.length = 0;
+
+	this.push = function(val) {
+		this.stack[this.length++] = val;
+		if(val > this.max) {
+			this.max = val;
+			this.maxArray.push(val);
+		}
+		return this.length;
+	}
+	this.pop = function() {
+		if(this.length === 0) return null;
+		const val = this.stack[--this.length];
+		delete this.stack[this.length];
+		if(this.maxArray[this.maxArray.length-1] === val) {
+			this.maxArray.pop();
+			this.max = this.maxArray[this.maxArray.length-1];
+		}
+		return val;
+	}
+	this.getMax = function() {
+		return this.max;
+	}
 }
+// const stk = new Stack();
+// stk.push(5);
+// stk.push(10);
+// console.log(stk.max);
+// console.log(stk.stack);
+// stk.pop();
+// console.log(stk.max);
+// stk.pop();
+// console.log(stk.max);
+// console.log(stk.stack);
 
 module.exports = Stack;
