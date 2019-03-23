cf5c2a2ef92fbe4d33496a0a85e7a37202062989
Adam <spincycle01@yahoo.com>
2019-03-23 09:59:46 -0400

    attempt at max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,33 @@
  */
 
 function Stack() {
-  // body...
+  this.storage = {};
+  this.length = 0;
 }
+
+Stack.prototype.push = function(value) {
+  this.storage[this.length] = value;
+  this.length += 1;
+}
+
+Stack.prototype.pop = function() {
+  const remove = this.storage[this.length - 1];
+  delete this.storage[this.length - 1];
+  this.length -= 1;
+  return remove;
+}
+
+Stack.prototype.getMax = function() {
+  let max = this.storage[0]
+  for (let i = 0; i < this.length; i++) {
+    if (max < this.storage[i]) {
+      max = this.storage[i];
+    }
+  }
+  return max
+}
+
+
+
 
 module.exports = Stack;
