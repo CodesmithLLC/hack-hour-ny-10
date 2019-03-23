656e6ea20e51f8b0ff42c2de841b5ec2671e3447
Jovan <jovankelly5@gmail.com>
2019-03-23 10:12:18 -0400

    max-stack v2

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -10,12 +10,15 @@ function Stack() {
   // body...
   this.stack = {}
   this.length = 0;
-  this.max;
+  this.max = {
+    value: undefined,
+    count: 0
+  }
 }
 
 Stack.prototype.push = function(value) {
-  if (!this.length) this.max = value;
-  else if (value > this.max) this.max = value;
+  if (!this.length || value > this.max.value) this.max = {value, count: 1};
+  else if (value === this.max.value) this.max.count++;
 
   this.stack[this.length] = value;
   return ++this.length;
@@ -25,12 +28,28 @@ Stack.prototype.pop = function() {
   if (!this.length) return undefined;
 
   const last = this.stack[this.length - 1];
-  delete this.stack[this.length--];
+  delete this.stack[--this.length];
+
+  this.max.count--;
+  if (this.length === 0) {
+    this.max = {value: undefined, count: 0}
+  } else if (this.length > 0 && this.max.count === 0) {
+    const newMax = {value: this.stack[0], count: 1};
+    for (let i = 1; i < this.length; i += 1) {
+      if (this.stack[i] > this.newMax.value) {
+        newMax = {value: this.stack[i], count: 1};
+      } else if (this.stack[i] === this.newMax.value) {
+        newMax.count++;
+      }
+    }
+  }
+
   return last;
 }
 
 Stack.prototype.getMax = function() {
-  return this.max;
+  return this.max.value;
 }
+
 
 module.exports = Stack;
