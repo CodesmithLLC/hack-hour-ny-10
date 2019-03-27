794b1701c06777ce9932308e179dec4be09e74bc
Tayvon Wright <tayvonwright@gmail.com>
2019-03-27 08:57:24 -0400

    reverseLL-HH

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -13,8 +13,33 @@ function Node(value) {
     this.next = null;
 }
 
-function reverseLinkedList(head) {
+// function reverseLinkedList(head) {
+//     let prev = null;
+//     let next = null;
+//     let curr = head;
+//     while(curr){
+//         next = curr.next;
+//         curr.next = prev;
+//         prev = curr;
+//         curr = next;
+//     }
+//     curr = prev;
+//     return curr;
+// }
 
+function reverseLinkedList(head, tail = null) {
+    if (!head) return tail;
+    let next = head.next;
+    head.next = tail;
+    tail = head;
+    return reverseLinkedList(next, tail);
 }
+// var node1 = new Node('1');
+// var node2 = node1.next = new Node('2');
+// var node3 = node2.next = new Node('3');
+// var node4 = node3.next = new Node('4');
+// var node5 = node4.next = new Node('5');
+
+// console.log(reverseLinkedList(node1));
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
