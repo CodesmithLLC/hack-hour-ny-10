ea1d8619187870397e4b54061a21aad31bab6d2d
Tayvon Wright <tayvonwright@gmail.com>
2019-03-29 19:57:00 -0400

    merge-arrHH

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -14,7 +14,17 @@
  */
 
 function mergeArrays(arr1, arr2) {
-
+  let result = [];
+  for(let i = j = 0; i + j < arr1.length + arr2.length;){
+    if ( arr1[i] <= arr2[j] || j > arr2.length - 1 ) { result.push(arr1[i++]) }
+    else if ( arr2[j] <= arr1[i]  || i > arr1.length - 1 ) { result.push(arr2[j++]); }
+  }
+  return result;
 }
+
+// var my_array = [3,4,6,10,11,15,21];
+// var another_array = [1,5,8,12,14,19];
+// console.log(mergeArrays(my_array, another_array));
+// console.log(mergeArrays([6], [1,5,8,11,12,14,15,19,21]))
 
 module.exports = mergeArrays;
