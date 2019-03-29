ff950f2322a8e6ef41fa22a3b08478afc1d1ad0f
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-03-29 09:52:07 -0400

    no array reshuffling, should be linear time, n+m space

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -7,14 +7,34 @@
  * var another_array = [1,5,8,12,14,19];
  *
  * mergeArrays(my_array, another_array); -> [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21]
- * 
- * BONUS: 
- * Complete in O(n) time  
+ *
+ * BONUS:
+ * Complete in O(n) time
  *
  */
 
 function mergeArrays(arr1, arr2) {
+  //initialize a pointer at the head of each array
+  //initialize an output array
+  //compare the values at each pointer
+  //add the smaller to the output array, repeat until one list is empty
+  const output = [];
+  let p1 = 0;
+  let p2 = 0;
 
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] < arr2[p2]) {
+      output.push(arr1[p1]);
+      p1++;
+    } else {
+      output.push(arr2[p2]);
+      p2++;
+    }
+  }
+  p1 < arr1.length
+    ? output.push(...arr1.slice(p1))
+    : output.push(...arr2.slice(p2));
+  return output;
 }
 
 module.exports = mergeArrays;
