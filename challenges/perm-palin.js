7288bfefd15c0a2fbac55043d46aedb39446f150
Bryan Costa <bryananthony.costa@gmail.com>
2019-03-30 09:56:47 -0400

    perm palin hack hour

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,22 @@
  */
 
 function permPalin(str) {
-	
+  if (!str) return false;
+  if (str.length === 1) return true;
+  const count = {};
+  let letter;
+  let sum = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    letter = str[i];
+    count[letter] = 0;
+    count[letter] += 1;
+  }
+  for (const countLetters in count) {
+    sum += count[countLetters];
+  }
+  return sum % 2;
 }
+
+console.log(permPalin("cbaba"));
 
 module.exports = permPalin;
