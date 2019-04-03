2aecb51888095a2a0ec4db61a6aeff553746f090
Adam <spincycle01@yahoo.com>
2019-04-03 09:42:08 -0400

    O(n2) approach to two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,15 @@
  */
 
 function twoSum(arr, n) {
-
+  //iterate through array with nested for loops
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === n) return true;
+    }
+  }
+  return false;
 }
+
+console.log(twoSum([1, 2, 3, 4, 5, 6], 11))
 
 module.exports = twoSum;
