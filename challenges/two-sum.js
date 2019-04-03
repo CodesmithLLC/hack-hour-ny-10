61632434b3a1f3fd63691123396651c079def768
Bo Peng <pb0928@gmail.com>
2019-04-03 09:34:48 -0400

    done with two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,17 @@
  */
 
 function twoSum(arr, n) {
-
+    arr = arr.sort((a, b) => { return a-b; })
+    let front = 0;
+    let back = arr.length-1;
+    while(front < back) {
+        const sum = arr[front] + arr[back];
+        if(sum < n) front++;
+        else if(sum > n) back--;
+        else return true;
+    } return false;
 }
+
+//console.log(twoSum([5,2,7,5,8,4], 14));
 
 module.exports = twoSum;
