d8ecd2208b43860c6f53aae91643111fe4b8d253
Tolga Mizrakci <tolgamizrakci@Tolgas-MacBook-Pro.local>
2019-04-03 09:39:29 -0400

    finished two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -2,8 +2,35 @@
  *  array that sum up to the target value; return false otherwise
  */
 
-function twoSum(arr, n) {
+// function twoSum(arr, n) {
+//   for (let i=0; i<arr.length; i++) {
+//     for (let x=i+1; x<arr.length; x++) {
+//       if (arr[i] + arr[x] === n) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
 
+// faster solution
+
+function twoSum(arr, n) {
+  let sums = [];
+  let hashTable = {};
+
+  for (let i=0; i<arr.length; i++) {
+
+    let sumMinusElement = n - arr[i];
+
+    if (hashTable[sumMinusElement] !== undefined) {
+      sums.push([arr[i], sumMinusElement])
+    }
+
+    hashTable[arr[i]] = arr[i]
+  }
+  if (sums.lenght) return true
+  else return false;
 }
 
 module.exports = twoSum;
