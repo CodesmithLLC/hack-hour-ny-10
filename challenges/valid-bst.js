109660ac1f32a3d2707dd448c41ecaa5c5c0a246
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-04-04 09:32:08 -0400

    second quick guess

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -13,9 +13,12 @@ function BinaryTree(val) {
 
 function validBST(tree) {
   if (tree === null) return true;
-  if (tree.left.value > tree.value || tree.right.value < tree.value)
-    return false;
-  return validBST(tree.left) && validBST(tree.right);
+  if (tree.left) {
+    return tree.left.value > tree.value ? false : validBST(tree.left);
+  }
+  if (tree.right) {
+    return tree.right.value > tree.value ? false : validBST(tree.right);
+  }
 }
 
 module.exports = { BinaryTree: BinaryTree, validBST: validBST };
