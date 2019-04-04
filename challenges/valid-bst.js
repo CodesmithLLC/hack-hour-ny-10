22a66214564f35375315fb06eae0f8af03b6f7b4
Bo Peng <pb0928@gmail.com>
2019-04-04 09:56:59 -0400

    done with valid bst

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -13,7 +13,27 @@ function BinaryTree(val) {
 }
 
 function validBST(tree) {
-
+    const arr = [];
+    BST(tree, arr);
+    for(let i = 0; i < arr.length - 1; i++) {
+        if(arr[i+1] < arr[i]) return false;
+    } return true;
 }
+
+function BST(tree, arr) {
+    if(tree.left !== null) BST(tree.left, arr);
+    arr.push(tree.value);
+    if(tree.right !== null) BST(tree.right, arr);
+}
+
+// const bt = new BinaryTree(5);
+// bt.left = new BinaryTree(3);
+// bt.right = new BinaryTree(7);
+// bt.left.left = new BinaryTree(2);
+// bt.left.right = new BinaryTree(4);
+// bt.right.left = new BinaryTree(6);
+// bt.right.right = new BinaryTree(8);
+// bt.right.right.right = new BinaryTree(7);
+// console.log(validBST(bt));
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
