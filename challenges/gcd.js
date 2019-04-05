a50d94a0a3972f3c5529173eb7546294a486cc21
luijocroussett <ljcroussett@gmail.com>
2019-04-05 09:47:56 -0400

    Completed gcd challenge

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,20 @@
  */
 
 function gcd(a, b) {
-
+  let decrement = 1;
+  let startPoint = 0;
+  if (a % b === 0) return b;
+  if (b % a === 0) return a;
+  if (a % 2 === 0 && b % 2 === 0) decrement = 2;
+  if (a > b) startPoint = Math.floor(b / 2);
+  else startPoint = Math.flooe(a / b);
+  console.log(startPoint);
+  for (let divisor = startPoint; divisor >= 1; divisor -= decrement) {
+    if (a % divisor === 0 && b % divisor === 0) return divisor;
+  }
 }
+
+console.log(gcd(11, 8));
+console.log(gcd(99, 2));
 
 module.exports = gcd;
