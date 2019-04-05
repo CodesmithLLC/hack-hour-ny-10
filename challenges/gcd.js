25382c3087b91eea635483bd85fe9aacad621f2d
Adam <spincycle01@yahoo.com>
2019-04-05 10:05:01 -0400

    attempt at gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,26 @@
  */
 
 function gcd(a, b) {
+  a = Math.abs(a);
+  b = Math.abs(b);
 
+  if (b > a) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
+  while (true) {
+    if (b === 0) return a;
+    a = a % b;
+    if (a === 0) return b;
+    b = b % a
+  }
 }
+
+
+console.log(gcd(10, 8)) //-> 2
+console.log(gcd(10, 9)) //-> 1
+console.log(gcd(9, 10)) //-> 1
+console.log(gcd(-15, 10)) //-> 1
 
 module.exports = gcd;
