875e32b9954056f37a901b59689c0813eae778ac
Nel Malikova <gunelmalikova@gmail.com>
2019-04-05 09:36:57 -0400

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,12 @@
  */
 
 function gcd(a, b) {
-
+  if (a < b) {
+    [a, b] = [b, a];
+  }
+  for (let i = b; i >= 1; i--) {
+    if (a % i === 0 && b % i === 0) return i;
+  }
 }
 
 module.exports = gcd;
