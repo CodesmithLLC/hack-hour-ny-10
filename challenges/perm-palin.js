9c57a7ebcfc6c215700881519eaae769d7ab16aa
luijocroussett <ljcroussett@gmail.com>
2019-04-05 11:46:00 -0400

    completed perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,20 @@
  */
 
 function permPalin(str) {
-	
+  if (str.length === 1) return true;
+  let dictionary = {};
+  str.split('').forEach(element => {
+    if (!dictionary[element]) dictionary[element] = 1;
+    else dictionary[element] += 1;
+  });
+  console.log(dictionary);
 }
+// for (let value in dictionary) {
+// }
+
+console.log(permPalin('abab'));
+console.log(permPalin('cbaba'));
+console.log(permPalin('cbac'));
+console.log(permPalin('a'));
 
 module.exports = permPalin;
