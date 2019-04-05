5975b9954c26c993c0abf48de7c08670424202dd
luijocroussett <ljcroussett@gmail.com>
2019-04-05 11:35:30 -0400

    solved rotate-grid

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -1,14 +1,14 @@
-/* You are given a NxN grid of elements represented by a 2D array. The ith nested array represents 
+/* You are given a NxN grid of elements represented by a 2D array. The ith nested array represents
  * the ith row in the grid.
- * 
- * Write a function to rotate the grid 90 degrees clockwise. 
+ *
+ * Write a function to rotate the grid 90 degrees clockwise.
  *
  * For example:     sampleGrid before:  [   [1, 2, 3],
  *                                          [4, 5, 6],
  *                                          [7, 8, 9]  ]
- *                  
+ *
  *                  rotateGrid(sampleGrid, 3);
- *                  
+ *
  *                  sampleGrid after:   [   [7, 4, 1],
  *                                          [8, 5, 2],
  *                                          [9, 6, 3]  ]
@@ -17,7 +17,21 @@
  */
 
 function rotateGrid(grid, n) {
-
+  let newGrid = [];
+  for (let index = 0; index < n; index++) {
+    newGrid.push([]);
+    for (let index2 = n - 1; index2 >= 0; index2--) {
+      newGrid[index].push(grid[index2][index]);
+    }
+  }
+  return newGrid;
 }
+
+// console.log(
+//   rotateGrid(
+//     [[1, 2, 3, 4], [12, 13, 14, 5], [11, 16, 15, 6], [10, 9, 8, 7]],
+//     4,
+//   ),
+// );
 
 module.exports = rotateGrid;
