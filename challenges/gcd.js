dff35fd96d23a59d1ef7026c5467485721ebf54f
Nel Malikova <gunelmalikova@gmail.com>
2019-04-05 10:53:02 -0400

    valid-bst

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,19 +7,19 @@
  *
  */
 
-// function gcd(a, b) {
-//   if (a < b) {
-//     [a, b] = [b, a];
-//   }
-//   for (let i = b; i >= 1; i--) {
-//     if (a % i === 0 && b % i === 0) return i;
-//   }
-// }
-// euclidian
 function gcd(a, b) {
-  if (b === 0) return a;
-  return gcd(b, a % b);
+  if (a < b) {
+    [a, b] = [b, a];
+  }
+  for (let i = b; i >= 1; i--) {
+    if (a % i === 0 && b % i === 0) return i;
+  }
 }
+// euclidian
+// function gcd(a, b) {
+//   if (b === 0) return a;
+//   return gcd(b, a % b);
+// }
 
 // console.log(gcd(10, 3));
 // console.log(gcd(10, 40));
--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -4,16 +4,68 @@
  *      at any given node, the value of all the nodes in its right tree must be > its value
  * Assume that each value in the tree is unique.
  */
- 
 
 function BinaryTree(val) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
+  this.value = val;
+  this.left = null;
+  this.right = null;
 }
 
 function validBST(tree) {
+  return isBST(tree, (min = -Infinity), (max = Infinity));
 
+  function isBST(tree, min, max) {
+    if (!tree) return true;
+
+    if (tree.value <= min || tree.value >= max) return false;
+
+    return (
+      isBST(tree.left, min, tree.value) && isBST(tree.right, tree.value, max)
+    );
+  }
 }
 
-module.exports = {BinaryTree: BinaryTree, validBST: validBST};
+// inorder traversal, inefficient!
+// function validBST(tree) {
+//   const values = [];
+//   // inorder traversal
+//   function traverse(root){
+//     if (!root) return;
+//     if (root.left) traverse(root.left);
+//     values.push(root.value);
+//     if (root.right) traverse(root.right);
+//   }
+//   traverse(tree);
+//   for (let i = 0; i < values.length; i += 1){
+//     if (values[i] >= values[i + 1]) return false;
+//   }
+//   return true;
+// }
+
+// efficient
+// function validBST(
+//   root,
+//   min = Number.NEGATIVE_INFINITY,
+//   max = Number.POSITIVE_INFINITY
+// ) {
+//   if (!root) return true;
+//   if (root.value <= min || root.value >= max) return false;
+//   return (
+//     validBST(root.left, min, root.value) &&
+//     validBST(root.right, root.value, max)
+//   );
+// }
+
+// const bt = new BinaryTree(10);
+// bt.left = new BinaryTree(8);
+// bt.right = new BinaryTree(12);
+// bt.left.left = 6;
+// bt.left.right = 9;
+// bt.right.left = 11;
+// bt.right.right = 13;
+
+// console.log(JSON.stringify(bt, null, 4));
+// console.log(bt);
+// console.log(validBST(bt));
+
+module.exports = { BinaryTree: BinaryTree, validBST: validBST };
