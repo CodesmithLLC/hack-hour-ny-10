387b807c7ffd797f4deaba4c462791a5e74a26eb
luijocroussett <ljcroussett@gmail.com>
2019-04-05 11:34:56 -0400

    solved two-sum and gcd

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -1,9 +1,17 @@
-/*  Given an array of numbers and a target number, return true if there are two numbers in the 
+/*  Given an array of numbers and a target number, return true if there are two numbers in the
  *  array that sum up to the target value; return false otherwise
  */
 
 function twoSum(arr, n) {
-
+  sumTwoArray = false;
+  let dictionary = {};
+  for (index = 0; index < arr.length; index++) {
+    dictionary[arr[index]] = n - arr[index];
+    if (dictionary[n - arr[index]]) return true;
+  }
+  return false;
 }
+
+// console.log(twoSum([1, 2, 4, 5], 3));
 
 module.exports = twoSum;
