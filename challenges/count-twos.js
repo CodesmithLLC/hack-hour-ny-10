0a586a0e857de60e9e83d1af56f462d196b49462
Tayvon Wright <tayvonwright@gmail.com>
2019-04-15 11:32:32 -0400

    countTwosHH

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -2,15 +2,25 @@
 // For example: countTwos(3) should be 1 because there are no 2s in the number one. There is One 2 in the number two and no 2s in the number three.
 // Each 2 should be counted. For instance,  the number 2222 has four 2s. 
 
-// countTwos(1);  -> 0
-// countTwos(3);  -> 1
-// countTwos(13);  -> 2
-// countTwos(1000);  -> 300
-// countTwos(11420);  -> 4483
+console.log(countTwos(1)); // -> 0
+console.log(countTwos(3));  // -> 1
+console.log(countTwos(13)); // -> 2
+console.log(countTwos(1000));  // -> 300
+console.log(countTwos(11420));  // -> 4483
 
 
 function countTwos(num) {
-
+  let count = 0;
+  for (let i = 1; i <= num; i++){
+    count = i
+          .toString()
+          .split('')
+          .reduce((a,b) => {
+            if (b === '2') a++;
+            return a;
+          }, count); 
+  }
+  return count;
 }
 
 module.exports = countTwos;
