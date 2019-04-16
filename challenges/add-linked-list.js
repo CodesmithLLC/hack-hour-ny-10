679ff944a3c37aea37fc5b47f96b96a9cc109173
luijocroussett <ljcroussett@gmail.com>
2019-04-16 10:39:07 -0400

    add-linked-list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -17,8 +17,67 @@ function Node(val) {
   this.next = null;
 }
 
-function addLinkedList(l1, l2) {
+// function addLinkedList(l1, l2) {
+//   let ll = new Node(l1.value + l2.value);
+//   let llresult = ll;
+//   let carry = 0;
+//   if (llresult.value > 9) {
+//     llresult.value -= 10;
+//     carry = 1;
+//   }
+//   l1 = l1.next;
+//   l2 = l2.next;
+//   while (l1 || l2) {
+//     llresult.next = new Node(l1.value + l2.value + carry);
+//     llresult = llresult.next;
+//     if (llresult.value > 9) {
+//       carry = 1;
+//       llresult.value -= 10;
+//     } else carry = 0;
+//     l1 = l1.next;
+//     l2 = l2.next;
+//   }
+//   if (carry) llresult.value = new Node(carry);
+//   return ll;
+// }
 
+function addLinkedList(l1, l2) {
+  let ll = new Node(0);
+  let llresult = ll;
+  let carry = 0;
+  while (l1 || l2) {
+    llresult.value += carry;
+    if (l1) {
+      llresult.value += l1.value;
+      l1 = l1.next;
+    }
+    if (l2) {
+      llresult.value += l2.value;
+      l2 = l2.next;
+    }
+    if (llresult.value > 9) {
+      carry = 1;
+      llresult.value -= 10;
+    } else carry = 0;
+    if (l1 || l2) {
+      llresult.next = new Node(0);
+      llresult = llresult.next;
+    }
+  }
+  if (carry) llresult.value = new Node(1);
+  return ll;
 }
 
-module.exports = {Node: Node, addLinkedList: addLinkedList};
+module.exports = { Node: Node, addLinkedList: addLinkedList };
+
+// let l1 = new Node(2);
+// l1.next = new Node(1);
+// l1.next.next = new Node(5);
+
+// let l2 = new Node(5);
+// l2.next = new Node(9);
+// l2.next.next = new Node(2);
+
+// console.log(l1);
+// console.log(l2);
+// console.log(addLinkedList(l1, l2));
