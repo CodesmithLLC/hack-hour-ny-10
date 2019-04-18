e63161747be48963b74cc1580c119ff4862760c2
Adam <spincycle01@yahoo.com>
2019-04-18 09:29:52 -0400

    attempt at repeat-number, O(n) time and space

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,22 @@
  */
 
 function repeatNumbers(array) {
-
+  const obj = {};
+  for (let el of array) {
+    if (!obj[el]) {
+      obj[el] = 1;
+    } else {
+      obj[el] += 1;
+    }
+  }
+  for(let key in obj) {
+    if (obj[key] === 2) {
+      return key;
+    }
+  }
 }
+
+const arr = [1, 2, 3, 4, 5, 6, 1]
+console.log(repeatNumbers(arr))
 
 module.exports = repeatNumbers;
