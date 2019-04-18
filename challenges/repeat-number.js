c81c29fb627bc6657eeee782fa3ff82bf2fc750e
Nel Malikova <gunelmalikova@gmail.com>
2019-04-18 09:46:04 -0400

    second attempt repeat-number

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -10,20 +10,25 @@
  *
  */
 
-function repeatNumbers(array) {
-  // let mid = Math.floor(array.length / 2);
-  // let min = 0;
-  // let max = array.length-1;
-
-  for (let i = 0; i < array.length - 1; i++) {
-    if (array[i] === array[i + 1]) {
-      return array[i];
-    }
+function factorial(n) {
+  if (n === 1 || n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
   }
-  return -1;
 }
 
-// const arr = [1, 2, 3, 4, 5, 6, 6, 7];
+function repeatNumbers(array) {
+  const fact = factorial(array.length - 1);
+  let multiply = 1;
+
+  for (let i = 0; i < array.length; i++) {
+    multiply *= array[i];
+  }
+  return multiply / fact;
+}
+
+// const arr = [1, 2, 3, 4, 4];
 // console.log(repeatNumbers(arr));
 
 module.exports = repeatNumbers;
