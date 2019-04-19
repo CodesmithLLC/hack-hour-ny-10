871fc988288c8f062e324fc50cb31a7152bff34f
Bryan Costa <bryananthony.costa@gmail.com>
2019-04-19 09:50:44 -0400

    hack hour merge ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -9,9 +9,20 @@
  * Do not assume the ranges are in order
  */
 
-
+const times = [[0, 1], [5, 3], [8, 4], [10, 12], [9, 10]];
 function mergeRanges(array) {
-
+  array.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+  const stack = [];
+  let endTime;
+  array.forEach((el) => {
+    if (!endTime || el[0] > endTime[1]) {
+      stack.push((endTime = el));
+    } else if (el[1] > endTime[1]) {
+      endTime[1] = el[1];
+    }
+  });
+  return stack;
 }
+console.log(mergeRanges(times));
 
 module.exports = mergeRanges;
