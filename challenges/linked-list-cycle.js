f58d2d0972676ddf4168c226202eaf94ee517589
Nel Malikova <gunelmalikova@gmail.com>
2019-04-20 09:50:36 -0400

    linked-list-cycle with pointers

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -27,34 +27,47 @@
  *
  */
 
-var Node = function(value) {
+const Node = function(value) {
   this.value = value;
   this.next = null;
 };
 
-function hasCycle(head) {
-  if (!head) return false;
-  const repeats = {};
-  let next = head.next;
-  repeats[head.value] = 1;
-  while (next) {
-    if (repeats.hasOwnProperty(next.value)) {
-      return true;
+// function hasCycle(head) {
+//   const repeats = {};
+//   while (head) {
+//     if (repeats.hasOwnProperty(head.value)) {
+//       return true;
+//     }
+//     repeats[head.value] = 1;
+//     head = head.next;
+//   }
+//   return false;
+// }
+
+const hasCycle = head => {
+  if (!head || !head.next) return false;
+
+  let slowPointer = head;
+  let fastPointer = head.next;
+
+  while (fastPointer) {
+    if (fastPointer.value === slowPointer.value) return true;
+    else {
+      slowPointer = slowPointer.next;
+      fastPointer = fastPointer.next.next;
     }
-    repeats[next.value] = 1;
-    next = next.next;
   }
   return false;
-}
+};
 
-const node1 = new Node(1);
-const node2 = (node1.next = new Node(2));
-const node3 = (node2.next = new Node(3));
-const node4 = (node3.next = new Node(4));
-const node5 = (node4.next = new Node(5));
-console.log(JSON.stringify(node1, null, 4));
-console.log(hasCycle(node1)); // false
-node5.next = node2;
-console.log(hasCycle(node1)); // true
+// const node1 = new Node(1);
+// const node2 = (node1.next = new Node(2));
+// const node3 = (node2.next = new Node(3));
+// const node4 = (node3.next = new Node(4));
+// const node5 = (node4.next = new Node(5));
+// console.log(JSON.stringify(node1, null, 4));
+// console.log(hasCycle(node1)); // false
+// node5.next = node2;
+// console.log(hasCycle(node1)); // true
 
 module.exports = { Node: Node, hasCycle: hasCycle };
