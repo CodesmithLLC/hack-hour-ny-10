264a7c0753a659333443c7510b052e790923d18b
Bryan Costa <bryananthony.costa@gmail.com>
2019-04-22 09:32:53 -0400

    hack hour multiples of 3 or 5

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -1,4 +1,3 @@
-'use strict';
 // If we list all the natural numbers below 10 that are multiples of 3 or 5,
 // we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
@@ -7,16 +6,21 @@
 
 function sumMultiples3Or5Below1000() {
   let sum = 0;
-
+  for (let i = 0; i < 1000; i += 1) {
+    if (i % 3 === 0) sum += i;
+    if (i % 5 === 0) sum += i;
+  }
   return sum;
 }
-
 
 // extension make it dynamic function that takes input x,y,z
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
   let sum = 0;
-
+  for (let i = 0; i < z; i += 1) {
+    if (i % x === 0) sum += i;
+    if (i % y === 0) sum += i;
+  }
   return sum;
 }
 
