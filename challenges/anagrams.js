d9e3b15ff723f41393d47452a359d6b23f90a3f9
Adam <spincycle01@yahoo.com>
2019-04-24 09:46:07 -0400

    an anagram attempt

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,25 @@
   */
 
 function anagrams(string) {
+  const arr = [];
+  const chars = string.split("");
 
+  function inner (str, newStr) {
+    if (!str.length) {
+      arr.push(newStr.join(""));
+    }
+    for (let i = 0; i < str.length; i++) {
+      let copy = str.slice();
+      copy.splice(i, 1)
+      inner(copy, newStr.concat(str[i]))
+    } 
+  }
+  inner(chars, []);
+  return arr;
 }
+
+const result = anagrams('abc');
+console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+
 
 module.exports = anagrams;
