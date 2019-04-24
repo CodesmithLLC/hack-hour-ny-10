0733bde3971705dc32fe38875e280c26edbdce5a
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-04-24 09:48:49 -0400

    backtracking, no duplicate avoidance

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -7,13 +7,20 @@
  */
 
 /**
-  * example:
-  * var result = anagrams('abc');
-  * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
-  */
+ * example:
+ * var result = anagrams('abc');
+ * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+ */
 
-function anagrams(string) {
-
+function anagrams(string, perm = '', output = []) {
+  if (!string) return output.push(perm);
+  for (let i = 0; i < string.length; i++) {
+    const cand = string[i];
+    string = string.slice(0, i) + string.slice(i + 1);
+    anagrams(string, perm + cand, output);
+    string = string.slice(0, i) + cand + string.slice(i);
+  }
+  return output;
 }
 
 module.exports = anagrams;
