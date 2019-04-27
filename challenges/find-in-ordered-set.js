8a6291ed2ded3098314f3cb4fd9149e31d8821eb
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-04-27 09:38:31 -0400

    bin search tree

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -9,10 +9,13 @@ findInOrderedSet(nums, 2);  -> false
 
  */
 
-
 function findInOrderedSet(arr, target) {
+  if (!arr.length) return false;
 
+  const cand = arr[Math.floor(arr.length / 2)];
+  if (cand === target) return true;
+  if (target > cand) return findInOrderedSet(arr.slice(Math.floor(arr.length / 2) + 1), target);
+  if (target < cand) return findInOrderedSet(arr.slice(0, Math.floor(arr.length / 2)), target);
 }
-
 
 module.exports = findInOrderedSet;
