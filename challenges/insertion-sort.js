8342c7e887cd3bb2c64c8400b274d0424f1b2f9b
Adam <spincycle01@yahoo.com>
2019-04-29 09:28:23 -0400

    insertion sort attempted

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,22 @@
 
 
 function insertionSort(array) {
+  let subArr = 1;
 
+  for (let i = 0; i < array.length; i++) {
+    for(let j = subArr - 1; j >= 0; j--) { 
+      let temp = array[j + 1];
+      if (temp < array[j]){
+        array[j + 1] = array[j]
+        array[j] = temp;
+      }
+    }
+    subArr += 1;
+  }
+  return array;
 }
+
+const arr = [6, 5, 3, 1, 8, 7, 2, 4];
+console.log(insertionSort(arr)) 
 
 module.exports = insertionSort;
