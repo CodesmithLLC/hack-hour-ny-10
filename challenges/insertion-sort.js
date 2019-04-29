4b66ef91dd870854f174551f8e26222e42fcf39e
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-04-29 09:14:20 -0400

    two whiles

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,18 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
-
 function insertionSort(array) {
-
+  let pointer = 1;
+  while (pointer < array.length) {
+    let cand = array[pointer];
+    let back = pointer - 1;
+    while (array[back] > array[back + 1]) {
+      [array[back], array[back + 1]] = [array[back + 1], array[back]];
+      back--;
+    }
+    pointer++;
+  }
+  return array;
 }
 
 module.exports = insertionSort;
