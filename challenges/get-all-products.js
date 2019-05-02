3d9d6c53de106285d79b4066e1175960a637f6bd
Nel Malikova <gunelmalikova@gmail.com>
2019-05-02 10:34:51 -0400

    get-all-products.js

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -9,8 +9,47 @@
  * do not use division, becuase zero might be in the array and you cannot divide by zero
  */
 
-function getAllProducts(array) {
+// function getAllProducts(array) {
+//   let productArray = [];
+//   let zeroCount = 0;
+//   let productWithoutZeros = 1;
 
+//   for (let num of array) {
+//     // if 0 in array, ignore and push increment zero count
+//     if (num === 0) zeroCount++;
+//     // else increase product
+//     else productWithoutZeros *= num;
+//   }
+
+//   // If more than 1 zero in array, return products will always be 0
+//   if (zeroCount > 1) return [0];
+//   // Else if one zero exists, product will be total product and 0;
+//   else if (zeroCount === 1) return [productWithoutZeros, 0];
+
+//   // Else if no zeros in array, find all products and return productArray
+//   for (let num of array) {
+//     productArray.push(productWithoutZeros / num);
+//   }
+//   return productArray;
+// }
+
+// get all products without division
+function getAllProducts(arr) {
+  if (!arr || arr.length === 0) return [0];
+
+  let output = new Array(arr.length);
+  output[0] = 1;
+  for (let i = 1; i < arr.length; i += 1) {
+    output[i] = output[i - 1] * arr[i - 1];
+  }
+
+  let right = 1;
+  for (let j = arr.length - 1; j >= 0; j -= 1) {
+    output[j] *= right;
+    right *= arr[j];
+  }
+
+  return output;
 }
 
 module.exports = getAllProducts;
