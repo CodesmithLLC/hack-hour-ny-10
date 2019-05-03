4334d02d7a8e8e1722f3c2d28a3bca58b368d36c
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-05-03 09:06:08 -0400

    dlink

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -2,10 +2,10 @@
 Create a doubly linked list with an add and remove method
  */
 
-function LinkedList() {
-  this.head = null;
-  this.tail = null;
-}
+// function LinkedList() {
+//   this.head = null;
+//   this.tail = null;
+// }
 
 function Node(val) {
   this.val = val;
@@ -16,15 +16,30 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
-};
+// LinkedList.prototype.add = function(val) {
+//   if (!this.head){
+//     this.head = new Node(val)
+//     return;
+//   }
+//   while (this.head.next){
+//     this.head = this.head.next
+//   }
+// };
+// /*
+// Removes the first node with the inputted value
+//  */
+// LinkedList.prototype.remove = function(val) {
+// };
 
-/*
-Removes the first node with the inputted value
- */
-LinkedList.prototype.remove = function(val) {
-  
-};
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  add() {}
+
+  remove() {}
+}
 
 module.exports = LinkedList;
