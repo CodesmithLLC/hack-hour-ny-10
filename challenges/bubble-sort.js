0e106452f36139a79af70bd9d7f87ce9b83fbc5b
Bryan Costa <bryananthony.costa@gmail.com>
2019-05-04 09:18:34 -0400

    bubble sort hack hour

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -2,10 +2,20 @@
 
 // https://en.wikipedia.org/wiki/Bubble_sort
 
-
-
 function bubbleSort(array) {
-
+  for (let i = 0; i < array.length - 1; i += 1) {
+    let bool = false;
+    for (let k = 0; k < array.length - i - 1; k += 1) {
+      if (array[k] > array[k + 1]) {
+        const greater = array[k];
+        array[k] = array[k + 1];
+        array[k + 1] = greater;
+        bool = true;
+      }
+    }
+    if (bool === false) return array;
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
