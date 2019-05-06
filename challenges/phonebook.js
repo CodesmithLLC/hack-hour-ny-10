3120b1260ebeae7c8102725cfe049090ba6f2db9
Nel Malikova <gunelmalikova@gmail.com>
2019-05-06 09:39:54 -0400

    phonebook.js

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -14,7 +14,7 @@
 * jazbooks are not always sorted...
 *
 * Develop a function that takes in a jazbook and a name, searches through the jazbook and
-* returns the persons phone number. If the person does not exists, return false.
+* returns the persons phone number. If the person does not exist, return false.
 *
 * How efficient can you make this?
 
@@ -25,18 +25,53 @@
 */
 
 //  return the number associated with the name in the jazbook
-function findName(jazbook, name) {
-
-}
+function findName(jazbook, name) {}
 
 // return an object literal representing the jazbook
-function makePhoneBookObject(jazbook){
+class makePhoneBookObject {
+  constructor(jazbook) {
+    this.phonebook = {};
+    jazbook.forEach(entry => {
+      this.phonebook[entry[0]] = entry[1];
+    });
+  }
 
+  add(name, number) {
+    this.phonebook[name] = number;
+    return this.phonebook;
+  }
+
+  lookup(name) {
+    if (this.phonebook.hasOwnProperty(name)) {
+      return this.phonebook[name];
+    } else return 'no such name in phonebook';
+  }
+
+  remove(name) {
+    if (this.phonebook.hasOwnProperty(name)) {
+      const toDelete = this.phonebook[name];
+      delete this.phonebook[name];
+      return toDelete;
+    } else return 'no such name in phonebook';
+  }
 }
+
+const jazbook = [
+  ['alex', '301-844-3421'],
+  ['jae', '301-844-1211'],
+  ['david', '301-844-0978'],
+  ['travis', '301-844-8505'],
+  ['jasmine', '1800-974-4539']
+];
+// const phonebook = new makePhoneBookObject(jazbook);
+// phonebook.add('nel', '1234567');
+// console.log(phonebook);
+// console.log(phonebook.remove('nel'));
+// console.log(phonebook.lookup('alex'));
 
 const objectToExport = {
   findName,
-  makePhoneBookObject,
+  makePhoneBookObject
 };
 
 module.exports = objectToExport;
