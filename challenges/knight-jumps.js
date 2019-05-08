4f0c79d222ce5237feb5267713d0d6a36641b001
Bo Peng <pb0928@gmail.com>
2019-05-08 09:48:02 -0400

    done with knight jumps

--- challenges/knight-jumps.js
+++ challenges/knight-jumps.js
@@ -10,8 +10,29 @@
 //  example input:
 // var str = "(4 5)"
 
-function knightjumps(str) {
-
+function knightjumps(str, func = (x, y) =>  x < 1 || x > 8 || y < 1 || y > 8) {
+    const x = Number.parseInt(str[1]);
+    const y = Number.parseInt(str[3]);
+    if(func(x, y)) return undefined;
+    const loc = getLoc(x, y);
+    return loc.reduce((acc, item) => {
+        if(!func(item[0], item[1])) acc++;
+        return acc;
+    }, 0);
 }
+
+function getLoc(x, y) {
+    const arr = [];
+    arr.push([x+1, y+2]);
+    arr.push([x-1, y+2]);
+    arr.push([x+1, y-2]);
+    arr.push([x-1, y-2]);
+    arr.push([x+2, y+1]);
+    arr.push([x+2, y-1]);
+    arr.push([x-2, y+1]);
+    arr.push([x-2, y-1]);
+    return arr;
+}
+//console.log(knightjumps('(1 5)'));
 
 module.exports = knightjumps;
