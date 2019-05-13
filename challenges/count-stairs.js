2bf72aeab9d9bff588ea400ad27c769c9fcdfad8
Adam <spincycle01@yahoo.com>
2019-05-13 09:39:27 -0400

    recursive count-stairs approach

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -13,9 +13,18 @@
  *
  * That is a total of 8 different ways to take 5 steps, given that you can take 1 or 2 steps at a time.
  */
+//helper recursive func
+function fib (num) {
+  if (num <= 1) {
+    return num;
+  }
+  return fib(num-1) + fib(num-2)
+}
 
 function countStairs(n) {
-
+  return fib(n + 1)
 }
+
+console.log(countStairs(5))
 
 module.exports = countStairs;
